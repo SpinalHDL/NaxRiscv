1e5da59492629225dc8b2932ea0a7f23ab19158a
Dolu1990 <charles.papon.90@gmail.com>
2021-12-27 18:39:47 +0100

    GShare now implement up down counters

--- src/main/scala/naxriscv/prediction/GSharePlugin.scala
+++ src/main/scala/naxriscv/prediction/GSharePlugin.scala
@@ -1,8 +1,10 @@
 package naxriscv.prediction
 
 import naxriscv.Fetch._
+import naxriscv.Frontend
 import naxriscv.Global._
 import naxriscv.fetch.{AlignerPlugin, FetchConditionalPrediction, FetchPlugin, FetchWordPrediction}
+import naxriscv.frontend.FrontendPlugin
 import naxriscv.interfaces.JumpService
 import naxriscv.prediction.Prediction._
 import naxriscv.utilities.Plugin
@@ -13,6 +15,7 @@ import spinal.lib.pipeline.Stageable
 class GSharePlugin(entries : Int,
                    historyWidth : Int,
                    insertAt : Int = 2,
+                   counterWidth : Int = 2,
                    readAsync : Boolean = false) extends Plugin with FetchConditionalPrediction with HistoryUser{
 
   val readAt = insertAt + (if(readAsync) 0 else -1)
@@ -21,13 +24,24 @@ class GSharePlugin(entries : Int,
 
   val setup = create early new Area{
     val fetch = getService[FetchPlugin]
+    val frontend = getService[FrontendPlugin]
     val branchContext = getService[BranchContextPlugin]
+    val aligner = getService[AlignerPlugin]
 
     fetch.retain()
+    frontend.retain()
     branchContext.retain()
 
-    getService[AlignerPlugin].addWordContext(
-      CONDITIONAL_TAKE_IT
+
+    val keys = new AreaRoot {
+      val GSHARE_COUNTER = Stageable(Vec.fill(SLICE_COUNT)(UInt(counterWidth bits)))
+    }
+
+    aligner.addWordContext(
+      keys.GSHARE_COUNTER
+    )
+    branchContext.dispatchWrite(
+      keys.GSHARE_COUNTER
     )
   }
 
@@ -35,20 +49,16 @@ class GSharePlugin(entries : Int,
     val fetch = getService[FetchPlugin]
     val branchContext = getService[BranchContextPlugin]
     val predictor = getService[DecoderPredictionPlugin]
+    val frontend = getService[FrontendPlugin]
     val BRANCH_HISTORY = getService[HistoryPlugin].keys.BRANCH_HISTORY
 
     val words = entries / SLICE_COUNT
-
-
-    //    val keys = new AreaRoot {
-//      val GSHARE_STRONG = Stageable(Bits(SLICE_COUNT bits))
-//    }
+    val keys = setup.keys
 
     def gshareHash(address : UInt, history : Bits) = address(SLICE_RANGE.high + 1, log2Up(words) bits).reversed ^ U(history).resized
 
     val mem = new Area{ //TODO bypass read durring write ?
-      val takeIt = Mem.fill(words)(CONDITIONAL_TAKE_IT)
-//      val strong = Mem.fill(words)(keys.GSHARE_STRONG)
+      val counter = Mem.fill(words)(keys.GSHARE_COUNTER)
     }
 
     val readCmd = new Area{
@@ -61,26 +71,42 @@ class GSharePlugin(entries : Int,
     val readRsp = new Area{
       val stage = fetch.getStage(insertAt)
 
-      readAsync match {
-        case false => stage(CONDITIONAL_TAKE_IT) := mem.takeIt.readSync(readCmd.address, readCmd.stage.isReady)
-        case true  => stage(CONDITIONAL_TAKE_IT) := mem.takeIt.readAsync(readCmd.address)
+      def readMem[T <: Data](mem : Mem[T], address : UInt = readCmd.address) = readAsync match {
+        case false => mem.readSync(address, readCmd.stage.isReady)
+        case true  => mem.readAsync(address)
       }
+      stage(keys.GSHARE_COUNTER) := readMem(mem.counter)
+    }
 
-//      stage(keys.GSHARE_STRONG) := mem.strong.readSync(readCmd.address, readCmd.stage.isReady)
+    val onDecompressed = new Area{
+      val stage = frontend.pipeline.decompressed
+
+      for(slotId <- 0 until Frontend.DECODE_COUNT) {
+        stage(CONDITIONAL_TAKE_IT, slotId) := stage(keys.GSHARE_COUNTER, slotId).map(_.msb).asBits
+      }
     }
 
     val onLearn = new Area{
       val ctx = branchContext.learnRead(branchContext.keys.BRANCH_FINAL)
       val hash = gshareHash(ctx.pcOnLastSlice, branchContext.learnRead(BRANCH_HISTORY))
 
-      val takeItPort = mem.takeIt.writePort
-      takeItPort.valid := branchContext.learnValid && branchContext.learnRead(predictor.keys.BRANCH_CONDITIONAL)
-      takeItPort.address := hash
-      takeItPort.data := branchContext.learnRead(CONDITIONAL_TAKE_IT)
-      takeItPort.data(ctx.pcOnLastSlice(SLICE_RANGE)) := ctx.taken
+      val counters = branchContext.learnRead(keys.GSHARE_COUNTER)
+      val updated = keys.GSHARE_COUNTER()
+      val incrValue = ctx.taken ? U(1) | U((1 << counterWidth)-1)
+      val overflow = False
+      for(sliceId <- 0 until SLICE_COUNT){
+        updated(sliceId) := counters(sliceId) + incrValue.andMask(ctx.pcOnLastSlice(SLICE_RANGE) === sliceId)
+        overflow setWhen(ctx.taken && counters(sliceId).msb && !updated(sliceId).msb || !ctx.taken && !counters(sliceId).msb && updated(sliceId).msb)
+      }
+
+      val counterPort = mem.counter.writePort
+      counterPort.valid := branchContext.learnValid && branchContext.learnRead(predictor.keys.BRANCH_CONDITIONAL) && !overflow
+      counterPort.address := hash
+      counterPort.data := updated
     }
 
     fetch.release()
+    frontend.release()
     branchContext.release()
   }
 }
