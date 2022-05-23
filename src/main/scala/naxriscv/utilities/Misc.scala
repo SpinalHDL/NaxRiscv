103c46f4dc4fff24a108545ed697b75f6da3a0ac
Dolu1990 <charles.papon.90@gmail.com>
2022-05-23 16:23:03 +0200

    Move pipeline into spinal.lib

--- src/main/scala/naxriscv/utilities/Misc.scala
+++ src/main/scala/naxriscv/utilities/Misc.scala
@@ -7,21 +7,6 @@ import spinal.lib._
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-object Misc {
-  def outsideCondScope[T](that : => T) : T = {
-    val t = AsyncThread.current
-    t.allowSuspend = false
-    val body = Component.current.dslBody  // Get the head of the current component symboles tree (AST in other words)
-    val ctx = body.push()                 // Now all access to the SpinalHDL API will be append to it (instead of the current context)
-    val swapContext = body.swap()         // Empty the symbole tree (but keep a reference to the old content)
-    val ret = that                        // Execute the block of code (will be added to the recently empty body)
-    ctx.restore()                         // Restore the original context in which this function was called
-    swapContext.appendBack()              // append the original symboles tree to the modified body
-    t.allowSuspend = true
-    ret                                   // return the value returned by that
-  }
-}
-
 object AddressToMask{
   def apply(address : UInt, size : UInt, width : Int) : Bits ={
     size.muxListDc((0 to log2Up(width)).map(i => U(i) -> B((1 << (1 << i)) -1, width bits))) |<< address(log2Up(width)-1 downto 0)
