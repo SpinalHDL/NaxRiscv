8e9ddcca19efbd3cb97fd68bfafa3d6bb0971fc3
Dolu1990 <charles.papon.90@gmail.com>
2022-07-14 19:06:30 +0200

    LsuPeripheralAxiLite4 is now pipelined

--- src/main/scala/naxriscv/lsu/LsuIoAxiLite4.scala
+++ src/main/scala/naxriscv/lsu/LsuIoAxiLite4.scala
@@ -10,7 +10,13 @@ class LsuPeripheralAxiLite4 extends Plugin{
   val logic = create late new Area{
     val cache = getService[LsuPlugin]
     val native = cache.peripheralBus.setAsDirectionLess
-    val axi = master(native.toAxiLite4())
+    val axiRaw = native.toAxiLite4()
+    val axi = master(cloneOf(axiRaw))
+    axi.aw << axiRaw.aw.halfPipe()
+    axi.w  << axiRaw.w.halfPipe()
+    axi.b.halfPipe()  >> axiRaw.b
+    axi.ar << axiRaw.ar
+    axi.r.halfPipe()  >> axiRaw.r
     AxiLite4SpecRenamer(axi)
   }
 }
