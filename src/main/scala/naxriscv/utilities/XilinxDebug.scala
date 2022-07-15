2757feb944687995ee6d11e6d70fb9a08617cdd7
Dolu1990 <charles.papon.90@gmail.com>
2022-07-15 17:14:11 +0200

    Add more probes to XilinxDebug

--- src/main/scala/naxriscv/utilities/XilinxDebug.scala
+++ src/main/scala/naxriscv/utilities/XilinxDebug.scala
@@ -1,8 +1,12 @@
 package naxriscv.utilities
+import naxriscv.{Frontend, ROB}
+import naxriscv.execute.ExecutionUnitBase
 import naxriscv.fetch._
+import naxriscv.frontend.FrontendPlugin
 import naxriscv.lsu.{DataCachePlugin, LsuPlugin}
 import naxriscv.misc.{CommitPlugin, MmuPlugin}
 import spinal.core._
+import spinal.lib.Timeout
 class XilinxDebug extends Plugin {
   val logic = create late new Area{
     def patch(that : Data) = that.addAttribute("mark_debug", "true")
@@ -15,6 +19,8 @@ class XilinxDebug extends Plugin {
       case p : LsuPlugin => {
         patch(p.logic.special.enabled)
         patch(p.logic.special.isIo)
+        p.logic.sq.regs.foreach(e => patch(e.data.inRf))
+        patch(p.logic.sq.ptr.commit)
       }
       case p : MmuPlugin => {
         patch(p.logic.refill.busy)
@@ -24,6 +30,32 @@ class XilinxDebug extends Plugin {
         patch(p.logic.reschedule.valid)
         patch(p.logic.reschedule.pcTarget)
         patch(p.logic.reschedule.trap)
+        patch(p.logic.reschedule.cause)
+        patch(p.logic.reschedule.reason)
+        val dcacheCmd = getService[DataCachePlugin].logic.cache.io.store.cmd
+        val icacheInv = getService[FetchCachePlugin].logic.invalidate
+        val flushingStuff = dcacheCmd.valid && dcacheCmd.flush || !icacheInv.done
+        def addTimeout(cycles : Int): Unit ={
+          val noCommit = Timeout(cycles)
+          when(p.onCommit().mask.orR || flushingStuff) {noCommit.clear()}
+          patch(noCommit.state.setName("noCommitTrigger" + cycles))
+        }
+        addTimeout(256)
+        addTimeout(2000)
+        patch(p.logic.whitebox.robToPc.valid)
+        patch(p.logic.whitebox.robToPc.robId)
+        p.logic.whitebox.robToPc.pc.foreach(patch)
+        p.logic.ptr.stage.apply(0 until Frontend.DECODE_COUNT)(Frontend.MICRO_OP).foreach(patch)
+        patch(p.logic.ptr.commit)
+        patch(p.logic.ptr.alloc)
+        patch(p.logic.ptr.free)
+        patch(p.logic.whitebox.commit)
+      }
+      case p : FetchPlugin => p.pipeline.stages.foreach(s => patch(s.valid))
+      case p : FrontendPlugin => p.pipeline.stagesSet.foreach(s => patch(s.valid))
+      case p : ExecutionUnitBase if p.euId == "EU0" => {
+        p.pipeline.stagesSet.foreach(s => patch(s.valid))
+        p.pipeline.stagesSet.foreach(s => patch(s(ROB.ID)))
       }
       case _ =>
     }
