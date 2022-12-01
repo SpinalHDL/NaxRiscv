ea161a195d6f20f3fa786a5e9c6ccfec005b115f
Dolu1990 <charles.papon.90@gmail.com>
2022-12-01 11:02:35 +0100

    Cmb boot

---
+++ src/main/scala/spinal/lib/bus/cmb/Cmb.scala
@@ -0,0 +1,18 @@
+package spinal.lib.bus.cmb
+
+
+/**
+  * Channels :
+  * read cmd   : RU, RS, MU
+  * read rsp   : OK, KO
+  * write cmd  : WI, WS, IV
+  * write data :
+  * write rsp  : OK, KO
+  * snoop cmd  : MS, MI
+  * snoop rsp  : OK
+  *
+  * DMA => RO, RT, WT
+  */
+case class Cmb() {
+
+}
