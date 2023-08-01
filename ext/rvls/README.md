b568927ac1fbc00aed17f5365723c0ef73c9128f
Dolu1990 <charles.papon.90@gmail.com>
2023-08-01 09:53:32 +0200

    Move spike model in its own repository

---
+++ ext/rvls/README.md
@@ -0,0 +1,5 @@
+# RVLS
+
+RVLS (RISCV Lock-Step) is a RISC-V trace checker using SPIKE as golden reference.
+
+WIP
--- ext/rvls/src/rvls.cpp
+++ ext/rvls/src/rvls.cpp
@@ -516,7 +516,7 @@ void checkFile(std::ifstream &lines){
 
 int main(){
     cout << "miaou3" << endl;
-    std::ifstream f("/media/data/open/riscv/VexRiscvOoo/trace.txt");
+    std::ifstream f("../../trace.txt");
     checkFile(f);
 }
 
