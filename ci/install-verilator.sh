02fdc7acdd5ee8a0609a819cc9b8a7e704c692cc
bi262934 <bi262934@gre069627>
2025-01-28 09:31:49 +0100

    ci: fix Verilator installation directory by copying all files
    Added a step to copy all Verilator build files to the specified installation directory to ensure proper setup. This resolves potential issues with missing files in /verilator-/

--- ci/install-verilator.sh
+++ ci/install-verilator.sh
@@ -11,9 +11,10 @@ if [ ! -e "$2/verilator-$1/bin/verilator" ]; then
     git checkout $1
     mkdir -p $2/verilator-$1
     # copy scripts
-    autoconf && ./configure --prefix="$2/verilator-$1" && make -j `nproc` 
+    autoconf && ./configure --prefix="$2/verilator-$1" && make -j `nproc`
+    cp -r * $2/verilator-$1/
     rm -rf ~/tools/verilator
-    cp -r "$2/verilator-$1" ~/tools/verilator
+    cp -r * ~/tools/verilator
 else
     echo "Using Verilator from cached directory."
     # Vérification que ~/tools/verilator est à jour
